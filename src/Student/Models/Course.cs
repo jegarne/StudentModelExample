7c09393809df65de5e9572f74322e2f5ed1fda51
Jeremy Garner <jegarne@gmail.com>
2017-03-13 19:02:09 -0500

    Student and Course model examples

---
+++ src/Student/Models/Course.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Models
+{
+    public class Course
+    {
+        private string name;
+        private int courseId;
+        private List<Student> students;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public int CourseId
+        {
+            get { return courseId; }
+            private set { courseId = value; }
+        }
+
+        public List<Student> Students
+        {
+            get { return students; }
+            private set { students = value; }
+        }
+    }
+}
---
+++ src/Student/Models/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Models
+{
+    public class Student
+    {
+        private string name;
+        private int studentId;
+        private int numberOfCredits = 0;
+        private double gpa = 0;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public int StudentId
+        {
+            get { return studentId; }
+            private set { studentId = value; }
+        }
+
+        public int NumberOfCredits
+        {
+            get { return numberOfCredits; }
+            set { numberOfCredits = value; }
+        }
+
+        public double Gpa
+        {
+            get { return gpa; }
+            set { gpa = value; }
+        }
+        
+        //public string Name { get; set;}
+        //public int StudentId { get; set; }
+        //public int Credits { get; set; } = 0;
+        //public double GPA { get; set; } = 0;
+    }
+}
